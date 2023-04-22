c22fc6b08a6591cc5c111087be7465232ef292b1
reo-fujimoto2653 <reo.kt.9th@gmail.com>
2023-04-22 22:07:09 +0900

    css test

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   mode: "jit",
   content: [
-    "./src/**/*.{js, ts, jsx, tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
